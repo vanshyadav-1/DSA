df122ebf8cf1b6ff0f326499f2a3c7b428ddf81c
Vansh Yadav <vanshsunilyadav19@gmail.com>
2026-09-22 13:17:22 +0530

    Time: 1 ms (82.94%) | Memory: 11.4 MB (82.71%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,57 +3,36 @@ public:
     string minWindow(string s, string t) {
         if (s.size() < t.size())
             return "";
-
         vector<int> need(128, 0);
-
-        // Frequency of characters required from t
         for (char c : t) {
             need[c]++;
         }
-
         int required = t.size();
         int left = 0;
-
         int minLength = INT_MAX;
         int start = 0;
-
         for (int right = 0; right < s.size(); right++) {
             char c = s[right];
 
-            // This character was still required
             if (need[c] > 0) {
                 required--;
             }
-
-            // Add c to the current window
             need[c]--;
-
-            // Window contains all characters of t
             while (required == 0) {
-
-                // Update minimum window
                 if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     start = left;
                 }
-
-                // Remove leftmost character
                 char leftChar = s[left];
-
                 need[leftChar]++;
-
-                // We removed a required character
                 if (need[leftChar] > 0) {
                     required++;
                 }
-
                 left++;
             }
         }
-
         if (minLength == INT_MAX)
             return "";
-
         return s.substr(start, minLength);
     }
 };
